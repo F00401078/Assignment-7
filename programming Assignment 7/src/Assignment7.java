d75443fa2c3e0b859e0ce3bf8504224f8c1330e3
F00401078 <nikesh4646@gmail.com>
2016-03-23 15:32:27 -0500

    initial commit

---
+++ programming Assignment 7/src/Assignment7.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class Assignment7 
+{
+
+	public static void main(String[] args) 
+	{
+		Scanner input = new Scanner(Assignment7.class.getResourceAsStream("input.data"));
+      //String fileContents = " ";
+		
+	while(input.hasNextLine())
+		{
+		String line = input.nextLine();
+		//String s = input.nextLine();
+		
+		if(line.contains("<"))
+		{
+			line = line.replaceAll("<", "");
+			line = line.replaceAll(">", "->");
+			 String[] run = line.split("/");
+			 
+			 line= run[0];
+		}
+	
+	//	String s1 = input.nextLine();
+		
+		
+		System.out.println(line);
+		}
+	}}
+
+
